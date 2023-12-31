9ee13d2c1289acebbd37014ca78186c0e3644e26
hrteccrown <155297886+hrteccrown@users.noreply.github.com>
2024-01-01 02:29:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -171,33 +171,33 @@ const buy = mongoose.model("Buy", buySchema);
 //     .catch((err) => console.log(err));
 
 // csv = fs.readFileSync("./client/public/ldata.csv");
-var array = csv.toString().split("\r");
-let csvData = [];
-let headers = array[0].split(",");
-for (let i = 1; i < array.length - 1; i++) {
-  let obj = {};
-  let str = array[i];
-  let s = "";
+//var array = csv.toString().split("\r");
+//let csvData = [];
+// let headers = array[0].split(",");
+// for (let i = 1; i < array.length - 1; i++) {
+//   let obj = {};
+//   let str = array[i];
+//   let s = "";
 
-  let flag = 0;
-  for (let ch of str) {
-    if (ch === '"' && flag === 0) {
-      flag = 1;
-    } else if (ch === '"' && flag == 1) flag = 0;
-    if (ch === "," && flag === 0) ch = "|";
-    if (ch !== '"') s += ch;
-  }
-  let properties = s.split("|");
-  if (properties.length !== headers.length) continue;
+//   let flag = 0;
+//   for (let ch of str) {
+//     if (ch === '"' && flag === 0) {
+//       flag = 1;
+//     } else if (ch === '"' && flag == 1) flag = 0;
+//     if (ch === "," && flag === 0) ch = "|";
+//     if (ch !== '"') s += ch;
+//   }
+//   let properties = s.split("|");
+//   if (properties.length !== headers.length) continue;
 
-  for (let j in headers) {
-    if (properties[j].includes(",")) {
-      obj[headers[j]] = properties[j].split(",").map((item) => item.trim());
-    } else obj[headers[j]] = properties[j];
-  }
+  // for (let j in headers) {
+  //   if (properties[j].includes(",")) {
+  //     obj[headers[j]] = properties[j].split(",").map((item) => item.trim());
+  //   } else obj[headers[j]] = properties[j];
+  // }
 
-  csvData.push(obj);
-}
+//   csvData.push(obj);
+// }
 
 function extractCost(costString) {
   let flag = false;
